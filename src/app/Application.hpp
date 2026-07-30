e504607e7ea97725cde2793aee20588d112ba9b9
Mohamed Deraz Nasr <mderaznasr@gmail.com>
2026-07-30 19:26:35 +0300

    update app hpp

--- src/app/Application.hpp
+++ src/app/Application.hpp
@@ -8,6 +8,8 @@
  * it does not need the full GLFW def yet
  */
 struct GLFWwindow; // forward declaration
+#include "renderer/Mesh.hpp"
+#include <memory>
 
 namespace rubiksim {
 // declares a class named Application
@@ -31,11 +33,21 @@ private:
   // openGL gives many resources int ids. rhese ids are not the resource itself
   // vBuffer and stored vbo id and vArray stores the vao ids
   // shaderProgram_ stores shader program ids
-  unsigned int vertexArray_;
-  unsigned int vertexBuffer_;
+  std::unique_ptr<Mesh> squareMesh_;
   unsigned int shaderProgram_;
 
   Application(const Application &) = delete;
   Application &operator=(const Application &) = delete;
 };
 } // namespace rubiksim
+  //
+
+/*
+ * Important C++ concept:
+ * std::unique_ptr<Mesh> means “this object uniquely owns a Mesh.”
+ * Unique means there should be exactly one owner.
+ * When the unique_ptr is destroyed, it automatically deletes the Mesh.
+ * This is RAII.
+ * You no longer need vertexArray_ and vertexBuffer_ in Application because Mesh
+ * owns those now.
+ */
